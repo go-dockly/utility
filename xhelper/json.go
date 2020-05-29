317bc72cf4f0817b803c7d7581ccdeac8ecc13a8
svn <godockly@gmail.com>
2020-05-29 14:22:48 +0200

    xhelper.ToJson

--- xhelper/json.go
+++ xhelper/json.go
@@ -9,3 +9,12 @@ func (h *Helper) FromJSON(b []byte, expected interface{}) {
 	err := json.Unmarshal(b, expected)
 	h.suite.Require().NoError(err)
 }
+
+// ToJSON encodes a struct into serialized json
+func (h *Helper) ToJSON(obj interface{}) []byte {
+	bytes, err := json.Marshal(obj)
+
+	h.suite.Require().NoError(err)
+
+	return append(bytes, 0xa)
+}
